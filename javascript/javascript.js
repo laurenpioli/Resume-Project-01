162f668884079c90849e999ab38396039343b8bb
Lauren Pioli <lauren.pioli@gmail.com>
2020-11-02 22:03:49 -0500

    age calc working with weeks

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -34,4 +34,55 @@ function question() {
 }
 
 
+// Age Recalculator
 
+    // Establish constants
+
+const secondsPerMin = 60;
+const minsPerHour = 60;
+const hoursPerDay = 24;
+const daysPerWeek = 7;
+const weeksPerYear = 52;
+
+    // Collect input from a guest
+
+function ageCalc() {
+    const inputAge = prompt('Please enter your age in years.');
+
+    console.log(inputAge);
+
+    console.log( typeof inputAge );
+    
+    // Convert inputs from a string to a number
+
+    const ageYears = parseInt(inputAge);
+    
+    console.log(ageYears);
+    
+    console.log( typeof ageYears );
+
+    const recalcAge = prompt("Please enter a period of time you'd like your age recalculated into.")
+    
+    if (recalcAge === 'weeks' ) {
+        let weeksAlive = ( ageYears * weeksPerYear) ;
+        console.log(weeksAlive);
+        document.querySelector("#agecalculation").innerHTML = `You have been alive for ${weeksAlive} weeks.`;
+
+    // } else if ( recalcAge === 'minutes' ) {
+    //     let ageAnswer = ( ageYears * minutesPerHour * hoursPerDay * daysPerWeek * weeksPerYear );
+
+    }
+    
+
+}
+        // JAVASCRIPT FEATURE Use a JavaScript function to perform a mathematical operation
+
+    // if (ageYears) {
+    //     if ( isNAN(ageYears) {
+    //     //  Create a message displaying the random number
+    //     console.log(`Please reload the page and provide an age number in numerical format.`);
+    //     // document.querySelector("#agecalculation").innerHTML = `${ageYears} is a random number from 1 to your number ${low}`;
+    // } else {
+    //     console.log(`.`);
+    //     // document.querySelector("#randomanswer").innerHTML = `Please reload the page and provide a number in numerical format.`;
+    // }
